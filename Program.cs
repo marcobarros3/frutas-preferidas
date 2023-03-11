f108feb8d17f5cc3d8e3384fda32f0f6c218d6c9
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2023-03-10 21:42:44 -0300

    minhas frutas preferidas

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("minhas frutas preferidas");
+Console.WriteLine("1.maçã");
+Console.WriteLine("2.goiaba");
+Console.WriteLine("3.mamão");
+Console.WriteLine("4.uva");
+Console.WriteLine("5.banana");
